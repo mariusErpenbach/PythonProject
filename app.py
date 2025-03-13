38c557e63fa5b558d92bf90279b42cbad2069b9c
Hawty1 <marius_erpenbach@web.de>
2025-03-13 23:56:42 +0100

    Flask Server Structure

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from productivity import calc_productivity, work_input_saving, best_productivity
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to the Productivity App!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
